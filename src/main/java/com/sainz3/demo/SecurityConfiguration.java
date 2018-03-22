0c83c9542f5a36a83354f7076fbd09d9449b01fb
danielasainz <dasainz@email.wm.edu>
2018-03-22 16:44:50 -0400

    fixed functionalities

--- src/main/java/com/sainz3/demo/SecurityConfiguration.java
+++ src/main/java/com/sainz3/demo/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.sainz3.demo;
 
 import org.springframework.context.annotation.Configuration;
-        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +21,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                 .formLogin().loginPage("/login").permitAll()
                 .and()
                 .httpBasic();
+
+    }
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth)
