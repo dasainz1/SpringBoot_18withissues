545a8467bce76ab95348b801569d6b3f092db682
danielasainz <dasainz@email.wm.edu>
2018-03-22 07:08:03 -0400

    Initial commit Problems running SecurityConfiguration.java

---
+++ src/main/java/com/sainz3/demo/SpringBoot18FinalApplication.java
@@ -0,0 +1,12 @@
+package com.sainz3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot18FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot18FinalApplication.class, args);
+	}
+}
